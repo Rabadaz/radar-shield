36bfb6e54c5a9871ef1657b04ed19ab6b0eacb56
Martin Scheuchenpflug <p42892@fh-hagenberg.at>
2026-03-16 13:46:13 +0100

    wip

--- IRS.py
+++ IRS.py
@@ -17,12 +17,12 @@ measurements = Queue()
 run_high_score = 0
 last_run_high_score = 0
 high_score = 0
-smoothed_speed = 0.0
 EMA_ALPHA = 0.3  # lower = more smoothing
 POST_RUN_DISPLAY_DURATION = 15  # seconds to show run result before switching to overall high score
 
 
 def read_sensor_to_buffer():
+    smoothed_speed = 0.0
     while True:
         mm = sensor.readLatestValue()
         if sensor.enabled:
